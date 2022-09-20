7878c39db4b2b861c1de79d45c2f2f6cb36d02a1
Роман <pangujailbreake58@gmail.com>
2022-09-21 00:17:09 +0300

    1.2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 public class Main {
     public static void main(String[] args)
     {
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
 
-        System.out.println("Hello world!");
+        System.out.println("Dog"+" = "+dog);
+        System.out.println("Сat"+" = "+cat);
+        System.out.println("Paper"+" = "+paper);
+
+        System.out.println("     ");
+
+        dog = dog+4;
+        cat = cat+4;
+        paper = paper+4;
+
+        System.out.println("Dog"+" = "+dog);
+        System.out.println("Сat"+" = "+cat);
+        System.out.println("Paper"+" = "+paper);
+
+        System.out.println("     ");
+        dog = (int) (dog-3.5);
+        cat = cat-1.6;
+        paper = paper-7639;
+
+        System.out.println("Dog"+" = "+dog);
+        System.out.println("Сat"+" = "+cat);
+        System.out.println("Paper"+" = "+paper);
+
     }
 }
